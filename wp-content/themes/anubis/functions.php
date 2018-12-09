6786581dd6cf3852e758207228a0c6ba64e6494a
zerodivide85 <hasani.rogers@gmail.com>
2018-12-09 18:07:12 -0500

    first commit

---
+++ wp-content/themes/anubis/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Twenty Seventeen functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ */
+
+wp_enqueue_script('bundle-js', get_theme_file_uri('/bundles/bundle.js'));
