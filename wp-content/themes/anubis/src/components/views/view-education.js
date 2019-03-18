4a7f88aee8af2be5f9389673685352a4e00e6b30
zerodivide85 <hasani.rogers@gmail.com>
2019-03-17 20:37:21 -0400

    added education

--- wp-content/themes/anubis/src/components/views/view-education.js
+++ wp-content/themes/anubis/src/components/views/view-education.js
@@ -4,18 +4,51 @@ import { PageViewElement } from '../helpers/page-view-element.js';
 import { StylesShared } from '../styles/shared.js';
 
 class ViewEducation extends PageViewElement {
+  static get properties() {
+    return {
+      _educationMarkup: {
+        type: String
+      }
+    }
+  }
+
+  firstUpdated() {
+    this._fetchEducation();
+  }
+
   render() {
+    const education = this._educationMarkup || 'Nothing yet';
+    const educationElm = this.shadowRoot.querySelector('.education');
+
+    if (educationElm) educationElm.innerHTML = education;
+
     return html`
       ${StylesShared}
       <style>
         section {
-            text-align: center;
+          text-align: center;
+          font-size: 2rem;
         }
       </style>
+      <h3>Education</h3>
       <section>
-        <h2>Education</h2>
+        <div class="education"></div>
       </section>
     `
+  }
+
+  async _fetchEducation() {
+    const education = await fetch('/?rest_route=/wp/v2/pages&slug=education')
+      .then(response => response.text())
+      .then(text => {
+        try {
+          return JSON.parse(text);
+        } catch(error) {
+          console.log(error);
+        }
+      });
+
+      this._educationMarkup = education[0].content.rendered;
   }
 }
 
